7835220274f04cf350b43b3924193119eb235164
Bùi Đức Anh <superda1997@gmail.com>
2022-09-12 17:37:39 +0700

    Add change network popup

--- src/components/HOC/hoc.js
+++ src/components/HOC/hoc.js
@@ -1,8 +1,17 @@
+import { CircularProgress, Dialog, styled, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
-import { useCallback } from 'react';
 import Web3 from 'web3/dist/web3.min';
 import FestakedWithReward from '../../artifacts/contracts/FestakedWithReward.sol/FestakedWithReward.json';
 import tokenContract from '../../artifacts/contracts/tokenContract/tokenContract.json';
+import DesignButton from '../common/DesignButton';
+
+const CustomDialog = styled(Dialog)`
+  & .MuiDialog-paper {
+    background: url('assets/images/background-dialog.png');
+    background-size: 100% 100%;
+    color: #f1e9dc;
+  }
+`;
 
 const withWallet = (OriginalComponent) => {
   function NewComponent(props) {
@@ -68,7 +77,7 @@ const withWallet = (OriginalComponent) => {
 
     const getyourStakedBalance = async () => {
       if (!!account) {
-        await stakingContract.methods.stakeOf(account).call((error, result) => {
+        stakingContract.methods.stakeOf(account).call((error, result) => {
           setYourStakedBalance((result ?? 0) / 1e18);
         });
       }
@@ -108,7 +117,7 @@ const withWallet = (OriginalComponent) => {
 
     const getBalance = async () => {
       if (!!account) {
-        await tokenNPO.methods.balanceOf(account).call((error, result) => {
+        tokenNPO.methods.balanceOf(account).call((error, result) => {
           setBalance(result);
         });
       }
@@ -120,76 +129,122 @@ const withWallet = (OriginalComponent) => {
       if (formVisibility) {
         getyourStakedBalance();
         getBalance();
-        await stakingContract.methods.name().call((error, result) => {
+        stakingContract.methods.name().call((error, result) => {
           setPoolName(result);
         });
-        await stakingContract.methods.stakingCap().call((error, result) => {
+        stakingContract.methods.stakingCap().call((error, result) => {
           setStakingCap(result / 1e18);
         });
-        await stakingContract.methods.stakedBalance().call((error, result) => {
+        stakingContract.methods.stakedBalance().call((error, result) => {
           setStakedBalance(result);
         });
-        await stakingContract.methods.stakedTotal().call((error, result) => {
+        stakingContract.methods.stakedTotal().call((error, result) => {
           setStakedTotal(result);
         });
-        await stakingContract.methods.withdrawStarts().call((error, result) => {
+        stakingContract.methods.withdrawStarts().call((error, result) => {
           // setEarlyWithdraw(new Date(result * 1000).toLocaleString())
           setEarlyWithdraw(result);
         });
-        await stakingContract.methods.stakingStarts().call((error, result) => {
+        stakingContract.methods.stakingStarts().call((error, result) => {
           setstakingStart(result);
         });
-        await stakingContract.methods.stakingEnds().call((error, result) => {
+        stakingContract.methods.stakingEnds().call((error, result) => {
           setstakingEnds(result);
         });
-        await stakingContract.methods.withdrawEnds().call((error, result) => {
+        stakingContract.methods.withdrawEnds().call((error, result) => {
           setMaturityAt(result);
         });
-        await stakingContract.methods.rewardState().call((error, result) => {
+        stakingContract.methods.rewardState().call((error, result) => {
           setRewardState(result);
         });
       }
     };
 
-    const getNetwork = useCallback(async () => {
+    const getNetwork = async () => {
       const netId = await web3.eth.net.getId();
       setFormVisibility(netId === Number(process.env.REACT_APP_NETWORK_VERSION));
-    }, [web3.eth.net]);
+    };
 
     useEffect(() => {
       getNetwork();
-    }, [getNetwork]);
+    }, []);
 
     useEffect(() => {
       getInfos();
     }, [account, formVisibility]);
 
+    const APP_NETWORK =
+      Number(process.env.REACT_APP_NETWORK_VERSION) === 97
+        ? {
+            chainName: 'Binance Smart Chain',
+            chainId: '0x61',
+            rpcUrls: ['https://data-seed-prebsc-2-s2.binance.org:8545'],
+            blockExplorerUrls: ['https://testnet.bscscan.com/'],
+            nativeCurrency: { name: 'BNB', symbol: 'BNB', decimals: 18 },
+          }
+        : {
+            chainName: 'BSC Testnet',
+            chainId: '0x38',
+            rpcUrls: ['https://bsc-dataseed.binance.org/'],
+            nativeCurrency: { name: 'ETH', symbol: 'ETH', decimals: 18 },
+          };
+
+    const handleConnectBinance = async () => {
+      try {
+        await window.ethereum.request({
+          method: 'wallet_switchEthereumChain',
+          params: [{ chainId: `0x${Number(process.env.REACT_APP_NETWORK_VERSION).toString(16)}` }],
+        });
+      } catch (error) {
+        if (error.code === 4902) {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [APP_NETWORK],
+          });
+        }
+      }
+    };
+
     return (
-      <OriginalComponent
-        formVisibility={formVisibility}
-        stakingContractAddr={stakingContractAddr}
-        account={account}
-        connectMM={connectMM}
-        onAccountChange={onAccountChange}
-        chain={chain}
-        poolName={poolName}
-        stakingCap={stakingCap}
-        stakedBalance={stakedBalance}
-        stakedTotal={stakedTotal}
-        setStakedBalance={setStakedBalance}
-        earlyWithdraw={earlyWithdraw}
-        yourStakedBalance={yourStakedBalance}
-        setYourStakedBalance={setYourStakedBalance}
-        stakingStart={stakingStart}
-        stakingEnds={stakingEnds}
-        maturityAt={maturityAt}
-        rewardState={rewardState}
-        stakingContract={stakingContract}
-        tokenNPO={tokenNPO}
-        getyourStakedBalance={getyourStakedBalance}
-        balance={balance}
-        getInfos={getInfos}
-      />
+      <>
+        <OriginalComponent
+          formVisibility={formVisibility}
+          stakingContractAddr={stakingContractAddr}
+          account={account}
+          connectMM={connectMM}
+          onAccountChange={onAccountChange}
+          chain={chain}
+          poolName={poolName}
+          stakingCap={stakingCap}
+          stakedBalance={stakedBalance}
+          stakedTotal={stakedTotal}
+          setStakedBalance={setStakedBalance}
+          earlyWithdraw={earlyWithdraw}
+          yourStakedBalance={yourStakedBalance}
+          setYourStakedBalance={setYourStakedBalance}
+          stakingStart={stakingStart}
+          stakingEnds={stakingEnds}
+          maturityAt={maturityAt}
+          rewardState={rewardState}
+          stakingContract={stakingContract}
+          tokenNPO={tokenNPO}
+          getyourStakedBalance={getyourStakedBalance}
+          balance={balance}
+          getInfos={getInfos}
+        />
+        <CustomDialog fullWidth maxWidth='xs' open={!formVisibility}>
+          <div className='flex flex-col items-center py-8'>
+            <CircularProgress style={{ color: 'rgb(150, 103, 64)' }} />
+            <Typography variant='h3' className='mt-4 mb-2'>
+              Wrong Network
+            </Typography>
+            <Typography className='mb-6'>Please switch network to continue</Typography>
+            <DesignButton onClick={handleConnectBinance} imageSize='small'>
+              BNB Chain
+            </DesignButton>
+          </div>
+        </CustomDialog>
+      </>
     );
   }
   return NewComponent;
