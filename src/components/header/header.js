c47f9ffc027ba31b99d873da475663cebead510f
Bùi Đức Anh <superda1997@gmail.com>
2022-09-13 18:01:02 +0700

    Minor

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,19 @@
-import React from "react"
-import "./header.css"
-import withWallet from "../HOC/hoc"
-function Header (props) {
-  const txLink = `https://bscscan.com/address/${props.account}#tokentxns`
+import React from 'react';
+import './header.css';
+function Header() {
+  const txLink = `https://bscscan.com/address/#tokentxns`;
 
   return (
-    <header className="header">
-      <img
-        className="logo"
-        src="https://imgur.com/Qxw1soD.jpeg"
-        alt="logo"
-      ></img>
-      <a href={txLink} className="links" target="_blank" rel="noreferrer">
+    <header className='header'>
+      <img className='logo' src='https://imgur.com/Qxw1soD.jpeg' alt='logo'></img>
+      <a href={txLink} className='links' target='_blank' rel='noreferrer'>
         Transaction
       </a>
-      <a href="https://staking.spores.app" className="links">
+      <a href='https://staking.spores.app' className='links'>
         Staking Options
       </a>
     </header>
-  )
+  );
 }
 
-export default withWallet(Header)
+export default Header;
