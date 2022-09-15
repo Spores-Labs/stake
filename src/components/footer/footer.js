8f69a5bd9ef84acce4e87000a40271a36e447d6a
Bùi Đức Anh <superda1997@gmail.com>
2022-09-15 15:26:51 +0700

    Change footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,97 +1,14 @@
-import React from "react"
-import "./footer.css"
-import sporesLogo from "../../sporesLogo.svg"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {
-  faYoutube,
-  faMedium,
-  faInstagram,
-  faPinterest,
-  faFacebook,
-  faTelegram,
-  faTwitter,
-  faLinkedin
-} from "@fortawesome/fontawesome-free-brands"
-function Footer () {
+import React from 'react';
+import './footer.css';
+import { AppBar, Container, Toolbar } from '@mui/material';
+function Footer() {
   return (
-    <div className="footer">
-      <ul className="menu listStyleNone">
-        <p>Menu</p>
-        <li>
-          <a href="https://launchpad.spores.app/">Launchpad</a>
-        </li>
-        <li>
-          <a href="https://marketplace.spores.app/">Marketplace</a>
-        </li>
-        <li>
-          <a href="https://republique.spores.app/">Republique</a>
-        </li>
-      </ul>
-      <div className="connect">
-        <p>Connect</p>
-        <ul>
-          <li>
-            <a href="https://spores.medium.com/">
-              <FontAwesomeIcon icon={faMedium} />
-            </a>
-          </li>
-          <li>
-            <a href="https://instagram.com/SporesNetwork">
-              <FontAwesomeIcon icon={faInstagram} />
-            </a>
-          </li>
-          <li>
-            <a href="https://pinterest.com/SporesNetwork">
-              <FontAwesomeIcon icon={faPinterest} />
-            </a>
-          </li>
-          <li>
-            <a href="https://facebook.com/SporesNetwork">
-              <FontAwesomeIcon icon={faFacebook} />
-            </a>
-          </li>
-        </ul>
-        <ul>
-          <li>
-            <a href="https://t.me/SporesOfficial">
-              <FontAwesomeIcon icon={faTelegram} />
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/Spores_Network">
-              <FontAwesomeIcon icon={faTwitter} />
-            </a>
-          </li>
-          <li>
-            <a href="https://www.youtube.com/channel/UC9pgjF_aWprnVodvxO7OPkA">
-              <FontAwesomeIcon icon={faYoutube} />
-            </a>
-          </li>
-          <li>
-            <a href="https://linkedin.com/company/SporesNetwork">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-          </li>
-        </ul>
-      </div>
-      <ul>
-        <p>Legal</p>
-        <li>
-          <a href="https://marketplace.spores.app/terms-of-use">Term of use</a>
-        </li>
-        <li>
-          <a href="https://marketplace.spores.app/privacy-policy">
-            Privacy Policy
-          </a>
-        </li>
-        <li>
-          <a href="https://marketplace.spores.app/cookie-statement">
-            Cookie Statement
-          </a>
-        </li>
-      </ul>
-      <img src={sporesLogo} />
-    </div>
-  )
+    <AppBar component='footer' position='static' style={{ background: '#000' }}>
+      <Toolbar component={Container} className='flex text-tiny md:text-sm justify-between custom-container'>
+        <img src='/assets/images/logo-header.png' alt='logo' className='h-3.5 md:h-7' />
+        <div>© 2021 CROS-All Rights Reserved</div>
+      </Toolbar>
+    </AppBar>
+  );
 }
-export default Footer
+export default Footer;
