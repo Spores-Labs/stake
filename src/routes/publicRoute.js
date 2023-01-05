212707e6e589547e3166677e5879b82b656c9678
Bùi Đức Anh <superda1997@gmail.com>
2023-01-05 17:07:27 +0700

    Minor

--- src/routes/publicRoute.js
+++ src/routes/publicRoute.js
@@ -1,17 +1,16 @@
 import StakeView from '../components/StakeView/StakeView';
-import { LeaderBoard } from '../components/LeaderBoard';
 
 const publicRoute = {
   stakeView: {
-    path: '/stake2/stake',
-    url: () => `/stake2/stake`,
+    path: '/',
+    url: () => `/`,
     element: <StakeView />,
   },
-  leaderBoard: {
-    path: '/stake2/leader-board',
-    url: () => `/stake2/leader-board`,
-    element: <LeaderBoard />,
-  },
+  // leaderBoard: {
+  //   path: '/stake2/leader-board',
+  //   url: () => `/stake2/leader-board`,
+  //   element: <LeaderBoard />,
+  // },
 };
 
 export default publicRoute;
